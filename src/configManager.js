1375f10339b1eb054e661dc385ddd283e6e939f7
CornWorld <cw@had.ink>
2022-04-25 15:00:15 +0800

    [Feat] Add a multi-file config manager

--- src/configManager.js
+++ src/configManager.js
@@ -1,12 +1,37 @@
 import fs from 'fs';
 
 export default class ConfigManager {
-    static parse(path="./config/global.json") {
+    static parse(path="./config/") {
+        if(path.slice(-1)!=='/') path+='/';
+        let config={};
         try {
-            return JSON.parse(fs.readFileSync(path).toString());
+            let configFiles=fs.readdirSync(path).filter((name)=>{
+                return fs.statSync(path+name).isFile() && name.search(/.json$/);
+            });
+            for(let name of configFiles) {
+                let configOrigin=JSON.parse(fs.readFileSync(path+name).toString());
+                let configName=name.slice(0,name.length-5);
+                switch (configName) {
+                    case 'global': {
+                        objMerge(config, configOrigin)
+                    } break;
+                    default: {
+                        config[configName]??={};
+                        objMerge(config[configName], configOrigin);
+                    } break;
+                }
+            }
+            
+            return config;
         }
         catch (err) {
             console.warn(err);
         }
+    }
+}
+
+function objMerge(to, from) {
+    for(let item in from) {
+        to[item]=from[item];
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import ConfigManager from "./configManager.js";
 import PluginManager from "./pluginManager.js";
 
 global.config=ConfigManager.parse();
+
+console.log(global.config);
+
 global.pluginManager=new PluginManager();
 global.plugins=global.pluginManager.pluginList;
 
